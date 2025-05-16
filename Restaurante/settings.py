238cc5ada3ed7996984e8f38a0dc8a5d85359d2d
FrankDev88 <memehair43@gmail.com>
2025-05-15 23:38:22 -0600

    hos

--- Restaurante/settings.py
+++ Restaurante/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = "django-insecure-mybzt79la@twtwvv700b&jsc2cifqmp%*l4=_vuqx6cs+=ufw^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pia-metodologias-abi.onrender.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'pia-metodologias-abi.onrender.com',
+    '.railway.app',  # permite cualquier subdominio de Railway
+]
 
 
 # Application definition
